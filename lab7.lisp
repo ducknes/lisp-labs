90e2ad90f87ac7e9b82058dd569f319aa5a42743
Ilya Antonov <i.antonov@smartway.today>
2023-11-26 15:07:10 +0300

    lr7: add format out

--- lab7.lisp
+++ lab7.lisp
@@ -1,6 +1,6 @@
 (defun sum-count(a i n)
-    if (= n 0)
+    (if (= n 0)
     0
-    (+ (* a i) (sum-count a (- i 1) (- n 1))))
+    (+ (* a i) (sum-count a (+ i 1) (- n 1)))))
 
-(write (sum-count 1 1 5))
+(format t "Сумма = ~d" (sum-count 1 1 5))
