538133e552d7c8ed04ad11c5fdbc1892e2843b1f
Ilya <112892994+ducknes@users.noreply.github.com>
2023-12-20 22:51:52 +0300

    change return

--- lab11.lisp
+++ lab11.lisp
@@ -1,14 +1,14 @@
 (defun checkSequence (lst curInd ssLen)
     (if (equal (- (length lst) (* ssLen 3)) curInd)
         (if (equal ssLen 6)
-            "False"
+            "NO"
             (checkSequence lst 0 (+ ssLen 1))
         )
         (if (and 
             (equal (subseq lst curInd (+ curInd ssLen)) (subseq lst (+ curInd ssLen) (+ curInd (* ssLen 2)))) 
             (equal (subseq lst (+ curInd ssLen) (+ curInd (* ssLen 2))) (subseq lst (+ curInd (* ssLen 2)) (+ curInd (* ssLen 3))))
             )
-            "True"
+            "YES"
             (checkSequence lst (+ curInd 1) ssLen)
         )
     )
